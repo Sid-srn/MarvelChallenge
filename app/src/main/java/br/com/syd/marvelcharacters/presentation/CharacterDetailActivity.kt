56b6fc0718c60465d89b7707759f9ff47e142b45
Sid-srn <syd.srn@gmail.com>
2022-03-17 00:55:10 -0300

    image

--- app/src/main/java/br/com/syd/marvelcharacters/presentation/CharacterDetailActivity.kt
+++ app/src/main/java/br/com/syd/marvelcharacters/presentation/CharacterDetailActivity.kt
@@ -1,26 +1,27 @@
 package br.com.syd.marvelcharacters.presentation
 
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import br.com.syd.marvelcharacters.R
+import br.com.syd.marvelcharacters.databinding.ActivityCharacterDetailBinding
 import br.com.syd.marvelcharacters.domain.model.CharacterModel
-import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
 
 class CharacterDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCharacterDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_character_detail)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_character_detail)
 
         val character = intent.getParcelableExtra<CharacterModel>("name_of_extra")
         val text = findViewById<TextView>(R.id.name)
-        val imgView = findViewById<ImageView>(R.id.detail_character_image)
-        text.text = character?.name?:""
-        //Picasso.with(this)
-        //    .load(character?.picture?.replace("portrait_small","portrait_medium")?:"")
-        //    .into(imgView);
-        Glide.with(this).load("http://i.imgur.com/DvpvklR.png").into(imgView);
-
+        binding.name.text = character?.name?:""
+        Picasso.with(this)
+            .load(character?.picture?.replace("portrait_small","standard_amazing")?:"")
+            .into(binding.detailCharacterImage)
     }
 }
